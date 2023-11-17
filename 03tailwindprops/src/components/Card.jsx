b1199e60657dccdf5d5e034015dad2efeb22428e
NikoloziChikvaidze <nikkchikvaidze@gmail.com>
2023-11-17 16:29:26 +0400

    created new component

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img className="w-24 h-24 rounded-full mx-auto" src="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj" alt="" width="384" height="512" />
+        <div className="pt-6 text-center space-y-4">
+        <blockquote>
+            <p className="text-lg font-medium">
+                “Tailwind CSS is the only framework that I've seen scale
+                on large teams. It’s easy to customize, adapts to any design,
+                and the build size is tiny.”
+            </p>
+        </blockquote>
+        <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+                Sarah Dayan
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+                Staff Engineer, Algolia
+            </div>
+        </figcaption>
+    </div>
+        </figure>
+    </div>
+  )
+}
+
+export default Card
